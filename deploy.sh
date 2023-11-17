f1171a468554c29bcc30e7d33faffdb72ff07fbe
shreegs123 <144917366+shreegs123@users.noreply.github.com>
2023-11-18 00:59:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,16 @@ echo "$remoteBranch"
 
 if [ $remoteBranch == 'dev' ]; then
     echo "Code pushed to dev branch. Building and pushing Docker image..."
-    export DOCKERHUB_PASSWORD= "dockerhub_password"
-    export DOCKERHUB_USERNAME= "dockerhub_username"
+   # export DOCKERHUB_PASSWORD= "dockerhub_password"
+   # export DOCKERHUB_USERNAME= "dockerhub_username"
     
  # Log in to DockerHub without requiring TTY
    # echo ${DOCKERHUB_PASSWORD} | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
-   docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
+  # docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
+
+withCredentials([string(credentialsId: ‘manjusecret’, variable: ‘dockerhubpwd’)]) {
+docker login -u manjusecret -p ${dockerhubpwd}
+docker push manju/javapp
 
 
     # Build the Docker image
