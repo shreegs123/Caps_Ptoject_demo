6e9fa3203a109e24cacc7d4846e5f3d0cebf15d3
shreegs123 <144917366+shreegs123@users.noreply.github.com>
2023-11-17 21:51:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-#!/bin/bash
-
 # Get the current local branch
 gitBranch=$(git rev-parse --abbrev-ref --symbolic-full-name HEAD | tr -d '\n')
 
@@ -10,5 +7,26 @@ remoteBranch=$(git ls-remote --heads origin | grep "$(git rev-parse HEAD)" | cut
 
 # Print the result
 echo "Git Branch: origin/$remoteBranch"
+
+DOCKERHUB_USERNAME="your_dockerhub_username"
+DOCKERHUB_PASSWORD="your_dockerhub_password"
+
+
+if [ remoteBranch == 'dev' ]; then
+    echo "Code pushed to dev branch. Building and pushing Docker image..."
+
+    # Build the Docker image
+    docker build -t "${DOCKERHUB_USERNAME}/dev-image:latest" .
+
+    # Log in to DockerHub
+    echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+
+    # Push the Docker image to DockerHub
+    docker push "${DOCKERHUB_USERNAME}/dev-image:latest"
+
+    echo "Docker image built and pushed successfully."
+else
+    echo "Code pushed to a branch other than dev. Skipping Docker image build."
+fi 
 
 
