e64cdf2ecfe49171211f09d95a5bb1643c885adf
shreegs123 <manjushreegs15@gmail.com>
2023-11-17 16:17:01 +0000

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-echo "env.BRANCH_NAME"
+#!/bin/bash
+
+# Get the current local branch
+gitBranch=$(git rev-parse --abbrev-ref --symbolic-full-name HEAD | tr -d '\n')
+
+# Get the corresponding remote branch
+remoteBranch=$(git ls-remote --heads origin | grep "$(git rev-parse HEAD)" | cut -d / -f 3 | tr -d '\n')
+
+# Print the result
+echo "Git Branch: origin/$remoteBranch"
 
 
