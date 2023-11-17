7684f0820883babbb46d0402a6cc29ce54b5891d
shreegs123 <144917366+shreegs123@users.noreply.github.com>
2023-11-17 20:42:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,4 @@
 
 echo "env.BRANCH_NAME"
 
-/*# DockerHub credentials
-DOCKERHUB_USERNAME="your_dockerhub_username"
-DOCKERHUB_PASSWORD="your_dockerhub_password"
-
-# Check if the branch is dev
-if [ env.BRANCH_NAME == 'dev' ]; then
-    echo "Code pushed to dev branch. Building and pushing Docker image..."
-
-    # Build the Docker image
-    docker build -t "${DOCKERHUB_USERNAME}/dev-image:latest" .
-
-    # Log in to DockerHub
-    echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-
-    # Push the Docker image to DockerHub
-    docker push "${DOCKERHUB_USERNAME}/dev-image:latest"
-
-    echo "Docker image built and pushed successfully."
-else
-    echo "Code pushed to a branch other than dev. Skipping Docker image build."
-fi */
 
