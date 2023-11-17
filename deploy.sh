729552696c7b5c2c4b8d1d98023a569beca7fdf0
shreegs123 <144917366+shreegs123@users.noreply.github.com>
2023-11-17 22:56:24 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,21 +9,18 @@ remoteBranch=$(git ls-remote --heads origin | grep "$(git rev-parse HEAD)" | cut
 echo "$remoteBranch"
 # echo "Git Branch: origin/$remoteBranch"
 
-# export DOCKERHUB_USERNAME="your_dockerhub_username"
-# export DOCKERHUB_PASSWORD="your_dockerhub_password"
-
 
 if [ $remoteBranch == 'dev' ]; then
     echo "Code pushed to dev branch. Building and pushing Docker image..."
 
     // Pull DockerHub credentials from Jenkins credentials
-    withCredentials([usernamePassword(credentialsId: DOCKERHUB_CREDENTIALS, usernameVariable: 'DOCKERHUB_USERNAME', passwordVariable: 'DOCKERHUB_PASSWORD')]) {
-    // Log in to DockerHub without requiring TTY
-    echo ${DOCKERHUB_PASSWORD} | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
+    withCredentials([usernamePassword(credentialsId: "${DOCKER_REGISTRY_CREDS}", passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+    echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin docker.io
 
-   /* # Log in to DockerHub
-    # echo "$DOCKERHUB_PASSWORD" | 
-    docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD} */
+    
+    // Log in to DockerHub without requiring TTY
+   # echo ${DOCKERHUB_PASSWORD} | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
+
 
     # Build the Docker image
     docker build -t "${DOCKERHUB_USERNAME}/dev-image:latest" .
