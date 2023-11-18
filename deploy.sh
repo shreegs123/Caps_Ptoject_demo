44bc2bbed9176e49230aa478ab5398c61ea5bcdf
shreegs123 <144917366+shreegs123@users.noreply.github.com>
2023-11-18 12:56:53 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,27 +11,18 @@ echo "$remoteBranch"
 
 
 if [ $remoteBranch == 'dev' ]; then
-    echo "Code pushed to dev branch. Building and pushing Docker image..."
-   # export DOCKERHUB_PASSWORD= "dockerhub_password"
-   # export DOCKERHUB_USERNAME= "dockerhub_username"
-    
- # Log in to DockerHub without requiring TTY
-   # echo ${DOCKERHUB_PASSWORD} | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
-  # docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
+echo "Code pushed to dev branch. Building and pushing Docker image..."
+DOCKER_ID= DOCKER_ID
+DOCKER_PASSWORD= DOCKER_PASSWORD
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_ID --password-stdin
 
-withCredentials([string(credentialsId: ‘manjusecret’, variable: ‘dockerhubpwd’)]) {
-docker login -u manjusecret -p ${dockerhubpwd}
-docker push manju/javapp
-}
+# Build the Docker image
+docker build -t "${DOCKER_ID}/dev-image:latest" .
 
+# Push the Docker image to DockerHub
+docker push "${DOCKER_ID}/dev-image:latest"
 
-    # Build the Docker image
-    docker build -t "${DOCKERHUB_USERNAME}/dev-image:latest" .
-
-    # Push the Docker image to DockerHub
-    docker push "${DOCKERHUB_USERNAME}/dev-image:latest"
-
-    echo "Docker image built and pushed successfully."
+echo "Docker image built and pushed successfully."
 else
     echo "Code pushed to a branch other than dev. Skipping Docker image build."
 fi 
