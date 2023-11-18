b3435f6920691849b847ab4a0fe42eebacecd275
shreegs123 <144917366+shreegs123@users.noreply.github.com>
2023-11-18 17:12:08 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,8 @@ remoteBranch=$(git ls-remote --heads origin | grep "$(git rev-parse HEAD)" | cut
 
 # Print the result
 echo "$remoteBranch"
-# echo "Git Branch: origin/$remoteBranch"
 
-
+# check if the code is pushed to dev branch on git hub then build and push to dockerhub dev repo
 if [ $remoteBranch == 'dev' ]; then
 echo "Code pushed to dev branch. Building and pushing Docker image..."
 DOCKER_ID= DOCKER_ID
@@ -20,10 +19,21 @@ echo $DOCKER_PASSWORD | docker login -u $DOCKER_ID --password-stdin
 docker build -t $DOCKER_ID/dev:latest .
 
 # Push the Docker image to DockerHub
-#docker push "${DOCKER_ID}"/dev:caps-img
 docker push $DOCKER_ID/dev:caps_img
 
-echo "Docker image built and pushed successfully."
+echo "Docker image built and pushed successfully to dev repo."
+
+# check if the code is pushed to dev branch and dev branch is merged to main branch on github then build and push to prod repo on dockerhub
+elif [ $remoteBranch == 'dev' ] && git log -n 1 --merges --pretty=%B | grep -q "Merge branch 'dev' into main"; then
+    echo "Merging dev into main detected. Pushing to prod repository."
+
+    # Build the Docker image for production
+    docker build -t $DOCKER_ID/prod:latest .
+
+    # Push the Docker image to the production repository
+    docker push $DOCKER_ID/prod:caps_img
+
+    echo "Docker image built and pushed successfully to prod repo."
 else
     echo "Code pushed to a branch other than dev. Skipping Docker image build."
 fi 
